2f5924b14e749bceb1e2f34cd8097a9332f5af4f
hinotruckofficial <hinotruckofficial@gmail.com>
2026-01-21 23:19:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ try {
     <title>Dealer Hino Jakarta | Authorized Hino Dealer</title>
 
     <!-- ✅ Deskripsi SEO -->
-    <meta name="description" content="Dealer Hino Jakarta Siap Memberikan Harga Terbaik Untuk Anda. Dealer Hino resmi Jakarta, Bekasi & Tangerang. Jual truk dan bus Hino, harga terbaik, kredit mudah & servis resmi.">
+    <meta name="description" content="Dealer Hino Jakarta Siap Memberikan Harga Terbaik Untuk Truk Hino Dan Bus Hino. Dealer Hino resmi Jakarta, Bekasi & Tangerang. Jual truk dan bus Hino, harga terbaik, kredit mudah & servis resmi.">
     <meta name="keywords" content="dealer hino, dealer hino jakarta, dealer hino jabodetabek, dealer hino jakarta barat, dealer hino jakarta timur, dealer hino jakarta utara, dealer hino jakarta selatan, Dealer Hino Jabodetabek, dealer hino bekasi, dealer hino depok, dealer hino bogor, dealer hino bandung, dealer resmi hino indonesia, sales hino, promo hino, harga truk hino, jual truk hino, kredit truk hino, cicilan truk hino, hino ready stock, stok unit hino terbaru, harga hino terbaru 2026, promo kredit hino, Dealer Hino Jabodetabek, hino tangerang, sales hino tangerang, harga truk hino tangerang, promo hino tangerang, kredit truk hino tangerang, dealer hino resmi tangerang, dealer hino cikupa, dealer hino cikupa tangerang">
     <meta name="author" content="Dealer Hino Jakarta">
 
@@ -57,7 +57,7 @@ try {
     <link rel="canonical" href="https://dealerhinojakartabarat.com/" />
 
     <!-- ✅ Tambahkan ini agar judul 'Dealer Hino Jakarta' muncul di atas domain (seperti di Indomobil Hino) -->
-    <meta name="application-name" content="Dealer Hino Ja">karta
+    <meta name="application-name" content="Dealer Hino Jakarta
     <meta name="apple-mobile-web-app-title" content="Dealer Hino Jakarta">
 
     <!-- ✅ Open Graph untuk tampilan di Google / Facebook / WhatsApp -->
